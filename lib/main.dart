1cc2fc90afbc09ccb08b1f8fdc0680d414890484
Steve Lovett <stephendlovett@gmail.com>
2023-03-08 20:05:26 -0800

    Add basic functionality and concept notes

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,97 @@
 import 'package:flutter/material.dart';
 
+// may also be written as: void main() => runApp(MyApp());
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+// Can be rebuilt
+// all classes must extend either StatelessWidget or StatefulWidget
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  // createState creates a State object at the location in the widget tree where the function
+  // is called. In this case in creates a State object in the widget tree at the location
+  // of MyAppState which returns the State of the associated MyApp class.
+  State<StatefulWidget> createState() {
+    return MyAppState();
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+// Persistent. MyAppState is named by convention but can be named anything.
+// therefore we must indicate the class that this class holds the State for.
+class MyAppState extends State<MyApp> {
+  // class-wide variables are called properties
+  var questionIndex = 0;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void answerQuestions() {
+    // setState forces the app to re-render the UI by calling the build method of the
+    // widget where it is called. However, Flutter will not re-render the entire UI
+    // but will only re-render what has changed.
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      questionIndex++;
     });
+    print(questionIndex);
   }
 
+  // decorator - clarifies that we are deliberately overriding the build method
   @override
+  // creates an object of type Widget that returns a MaterialApp Widget and
+  // calls the build function that tells Flutter what/when to render on the screen
+  // Note: all widgets are classes
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
+    return MaterialApp(
+      // home is a named argument of the MaterialApp Widget
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App!'),
         ),
+        // Column accepts a named argument children which can take a list of items
+        // in this case it takes a list of Widgets. Type inference would have identified
+        // this as a list of a Widgets, but this explicitly declares the data type.
+        body: Column(children: <Widget>[
+          Text(questions[questionIndex]),
+          ElevatedButton(
+            child: Text('Answer 1'),
+            // onPressed takes a function. However, if you were to write answerQuestion() instead,
+            // dart would call the function immediately while parsing the file and NOT when the user
+            // presses the button. We want this function to run when the user presses the button so we
+            // instead add answerQuestion as a reference to the function and not the return value of the
+            // function which in this case would be null since answerQuestion returns nothing or 'void'.
+            // answerQuestion() - return value of the function
+            // answerQuestion - pointer to the function itself
+            onPressed: answerQuestions,
+          ),
+          ElevatedButton(
+            child: Text('Answer 2'),
+            onPressed: answerQuestions,
+          ),
+          ElevatedButton(
+            child: Text('Answer 3'),
+            onPressed: answerQuestions,
+          ),
+        ]),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+/* NOTES
+Shorthand notation for a one-time use function is () => <CODE>;
+  or () {
+    <CODE>;
+  }
+  This is used when you only want to call a function once and never reference it 
+  again anywhere else in the code which would be impossible since it has no name.
+
+Stateless widgets rerender when external input changes
+Stateful widgets rerender when external input changes OR the internal state changes.
+
+State persists. The StatefulWidget class is recreated when it is rerendered, but
+the State class persists the data through rerendering.
+
+
+  */
